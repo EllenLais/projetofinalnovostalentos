c7af1810621688a1a25d01e7ae82df307cbbe4c6
EllenLais <ellen.laisb@gmail.com>
2022-11-10 14:19:55 -0300

    first commit

---
+++ src/main/java/br/univille/projetofinalnovostalentos/ProjetofinalnovostalentosApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projetofinalnovostalentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofinalnovostalentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofinalnovostalentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/projetofinalnovostalentos/ProjetofinalnovostalentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetofinalnovostalentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofinalnovostalentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
